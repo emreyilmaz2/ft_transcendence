d86c51831f92d4012d5d56f98ad3dd49be3449d3
Emre Yilmaz <emreeyilmaz567@gmail.com>
2024-03-12 09:50:32 +0300

    password authentication

--- ping_pong/serializers.py
+++ ping_pong/serializers.py
@@ -89,14 +89,12 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
         return attrs
 
     def validate_old_password(self, value):
-        user = self.context['request'].user
-        if not user.check_password(value):
+        current_user = self.context['request'].user
+        if not current_user.check_password(value):
             raise serializers.ValidationError({"old_password": "Old password is not correct"})
         return value
 
     def update(self, instance, validated_data):
-
         instance.set_password(validated_data['password'])
         instance.save()
-
         return instance
