8be30c3fb2467a5c6de7767a7d1bd2004b41ca22
Emre Yilmaz <emreeyilmaz567@gmail.com>
2024-03-27 09:36:48 +0300

    profile update 10/10

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -96,13 +96,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public'),]
 
 DEFAULT_USER_AVATAR = '/images/user.png'
 
