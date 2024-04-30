0f0eccdff7150e7bde5b43df59f1be04ed75712d
Emre Yilmaz <emreeyilmaz567@gmail.com>
2024-04-30 13:55:02 +0300

    Eski kodları temizle

--- ping_pong/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, FriendRequest, Match
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(FriendRequest)
-admin.site.register(Match)
